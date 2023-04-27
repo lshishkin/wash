49987e82328200d10df1c3414c36736e7cc172e7
lshishkin <lshishkin150586@gmail.com>
2023-04-27 22:31:01 +0500

    добавлены метатэги гугл и яндекс

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,8 @@ export default function Home() {
         <title>Мойка грузовых авто в Верхней Пышме | Чистота и качество - наша главная задача</title>
         <meta name="description" content="Мы предоставляем услуги мойки грузовых автомобилей в Верхней Пышме. В нашей мойке Вы можете рассчитывать на качественное обслуживание и полную чистоту Вашего авто." />
         <meta name="keywords" content="мойка грузовых авто, мойка грузовиков, мойка тяжелых авто, Верхняя Пышма" />
+        <meta name="yandex-verification" content="880f1616064e7d50" />
+        <meta name="google-site-verification" content="OOwAqqHsj7s9EtuDFcmO37CrXC5vtiRXz-sgg9RBgyI" />
         <link rel="icon" href="/wash.ico" />
       </Head>
       <MainPage />
